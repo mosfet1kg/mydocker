aaa838352719e069e9fd2e9eea778252bc7af85b
gbchoi <gb.choi@navercorp.com>
2018-01-12 15:22:37 +0900

    first commit

---
+++ controllers/Helloworld.ts
@@ -0,0 +1,6 @@
+export function sayhelloUsingGET( req, res, next ) {
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ res: 'hello' }));
+}
