beee7722810dfe6e489d0f090d3e43b459def33a
gbchoi <gb.choi@navercorp.com>
2018-01-12 20:09:23 +0900

    second commit

--- controllers/Helloworld.ts
+++ controllers/Helloworld.ts
@@ -4,3 +4,11 @@ export function sayhelloUsingGET( req, res, next ) {
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify({ res: 'hello' }));
 }
+
+
+export function sayhelloTestUsingGET( req, res, next ) {
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ res: 'hello' }));
+}
--- index.ts
+++ index.ts
@@ -1,13 +1,59 @@
-const app = require('connect')();
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
-const swaggerTools = require('swagger-tools');
-const jsyaml = require('js-yaml');
 const cors = require('cors');
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
+
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+const app = express();
+const v2Router = express.Router();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+app.use('/v2', v2Router);
+
+v2Router.route('/hello')
+  .get( (req, res, next) => {
+    res.send('hello');
+  });
+
+v2Router.route('/getPersonByKey/:key([^/]+/[^/]+)')
+  .get( (req, res, next) => {
+    console.log( req.params);
+    res.send('hello2');
+  });
+
+
+
+// app.get('/hi/*', (req, res, next) => {
+//
+//   res.send('hello');
+// });
+
+app.get('/hi/:id*/hello/:path*', (req, res, next) => {
+  console.log( req.params );
+  res.send('hello1');
+});
+
+
+app.get('/hi/*/hello', (req, res, next) => {
+
+  console.log( req.params );
+  res.send('hello2');
+});
+
+app.listen(10010, (err: Error) => {
+  console.log( err );
+});
 
 // const sslOptions = {
 //   key: fs.readFileSync( path.join(__dirname,'sslcert/all_ncloud_com_key.pem') ),
@@ -15,71 +61,38 @@ const helmet = require('helmet');
 //   passphrase: "ncloud!@#123"
 // };
 
-// swaggerRouter configuration
-const options = {
-  swaggerUi: '/swagger.json',
-  controllers: './controllers',
-  useStubs: false, // process.env.NODE_ENV === 'local' // Conditionally turn on stubs (mock mode)
-};
 
 // const allowedOrigin = [/\.ncloud\.com/];  // 포트가 달라도 허용 /\.ncloud\.com$/]
 // if ( process.env.NODE_ENV === 'local' ) {
 //   allowedOrigin.push(/localhost/);
 // }
 
-// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-const spec = fs.readFileSync('./api/swagger.yaml', 'utf8'); // .replace('localhost:10010', process.env.API_HOST + ":" + process.env.API_PORT );
-const serverPort = 10010; //process.env.API_PORT;
-
-const swaggerDoc = jsyaml.safeLoad(spec);
-
-// Initialize the Swagger middleware
-swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
-
-  if( process.env.NODE_ENV !== 'local' ) {
-    app.use( helmet() );
-    app.use( compression() );
-  }
-
-  app.use( cookieParser() );
-  app.use( cors({ origin: /\.ncloud\.com/, credentials: true }) );
-
-
-  // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
-  app.use(middleware.swaggerMetadata());
-
-  // Validate Swagger requests
-  app.use(middleware.swaggerValidator());
-
-  // Serve the Swagger documents and Swagger UI
-  app.use(middleware.swaggerUi());  /*** 권한때문에 auth 체크 앞에 위치하도록 해야함 **/
-
-  /** 권한 확인을 위한 middleware **/
-  app.use( ( req,  res, next ) => {
-    next();
-  });
-
-  /** Route validated requests to appropriate controller **/
-  app.use( middleware.swaggerRouter(options) );
-  /** For Error Handling **/
-  // app.use( errorHandling );
-
-  // if ( process.env.NODE_ENV !== 'local' ) {
-  //   https.createServer(sslOptions, app).listen(serverPort, ()=>{
-  //     console.log( `>>>>>>>>>>>>[ ${process.env.NODE_ENV} MODE ]<<<<<<<<<<<<<`);
-  //   });
-  //
-  //   http.createServer(app).listen(80, function () {
-  //     console.log( `>>>>>>>>>>>>[ ${process.env.NODE_ENV} MODE ]<<<<<<<<<<<<<`);
-  //     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-  //     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
-  //   });
-  // } else {
-    // Start the server
-  http.createServer(app).listen(serverPort, function () {
-    console.log( `>>>>>>>>>>>>[ ${process.env.NODE_ENV} MODE ]<<<<<<<<<<<<<`);
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
-  });
-  // }
-});
+//
+// if( process.env.NODE_ENV !== 'local' ) {
+//   app.use( helmet() );
+//   app.use( compression() );
+// }
+//
+// app.use( cookieParser() );
+// app.use( cors({ origin: /\.ncloud\.com/, credentials: true }) );
+//
+//
+// // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+// app.use(middleware.swaggerMetadata());
+//
+// // Validate Swagger requests
+// app.use(middleware.swaggerValidator());
+//
+// // Serve the Swagger documents and Swagger UI
+// app.use(middleware.swaggerUi());  /*** 권한때문에 auth 체크 앞에 위치하도록 해야함 **/
+//
+// /** 권한 확인을 위한 middleware **/
+// app.use( ( req,  res, next ) => {
+//   next();
+// });
+//
+// /** Route validated requests to appropriate controller **/
+// app.use( middleware.swaggerRouter(options) );
+// /** For Error Handling **/
+// // app.use( errorHandling );
+//
