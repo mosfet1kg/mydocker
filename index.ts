8b4f18df18fb98b2d477355bf1e57c075eed8132
gbchoi <gb.choi@navercorp.com>
2018-01-15 18:40:01 +0900

    update

--- index.ts
+++ index.ts
@@ -42,9 +42,9 @@ app.all('/v2/$', (req, res, next) => {
   // 404
 
   const credentials = auth(req);
-
+  console.log( credentials );
   if (!credentials || credentials.name !== 'john' || credentials.pass !== 'secret') {
-    res.statusCode = 401
+    res.statusCode = 401;
     res.setHeader('WWW-Authenticate', 'Basic realm="example"')
     res.end('Access denied')
   } else {
